b47281406e1720aec987d95fc1a5bf14a04a7e1e
faishalarif2 <faishalarifn02@gmail.com>
2022-04-08 11:22:12 +0700

    Commit

--- app/src/main/java/com/example/kelasasqlite/MainActivity.java
+++ app/src/main/java/com/example/kelasasqlite/MainActivity.java
@@ -52,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<daftarTeman.size();i++){
             Teman teman = new Teman();
 
-            teman.setId(daftarTeman.get(i).get("id").toString());
-            teman.setNama(daftarTeman.get(i).get("nama").toString());
-            teman.setTelpon(daftarTeman.get(i).get("telpon").toString());
+            teman.setId(daftarTeman.get(i).get("id"));
+            teman.setNama(daftarTeman.get(i).get("nama"));
+            teman.setTelpon(daftarTeman.get(i).get("telpon"));
             temanArrayList.add(teman);
         }
     }
