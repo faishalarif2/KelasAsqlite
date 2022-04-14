492cffcd665973a830d09817dab1cdb258d0f6c1
faishalarif2 <faishalarifn02@gmail.com>
2022-04-14 10:27:43 +0700

    tambahan

--- app/src/main/java/com/example/kelasasqlite/database/DBController.java
+++ app/src/main/java/com/example/kelasasqlite/database/DBController.java
@@ -57,4 +57,19 @@ public class DBController extends SQLiteOpenHelper {
         return daftarTeman;
     }
 
+    public void UpdateData(HashMap<String,String> queryValues){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues nilai = new ContentValues();
+        nilai.put("nama", queryValues.get("nama"));
+        nilai.put("telpon", queryValues.get("telpon"));
+        db.update("teman", nilai, "id=?",new String[]{queryValues.get("id")});
+        db.close();
+    }
+
+    public void DeleteData(HashMap<String,String> queryValues){
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete("teman","id=?",new String[]{queryValues.get("id")});
+        db.close();
+    }
+
 }
